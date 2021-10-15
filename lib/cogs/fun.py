b914a57b09206be20f115d7eab28de44674c6846
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2021-10-15 11:41:15 +0300

    Update fun.py

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -22,14 +22,13 @@ class Fun(Cog):
     async def slap_member(self, ctx, member:Member, *, reason: Optional[str] = "no reason"):
         await ctx.send(f"{ctx.author.name} ударил {member.mention} по причине: {reason}!")
       
-    @command(name="хелп", aliases=["помощь","Помощь"])
+    @command(name="help", aliases=["помощь","Помощь","хелп"])
     async def help(self, channel):
         embed = Embed(title="Команды:", colour=0x00FF00,
                           timestamp=datetime.utcnow())
         fields = [("Префикс: `>`","Модуль fun:", "Связь (Бот, связь, бот) - проверка связи с ботом", "slap (hit, ударить) `>slap @участник причина`", "Помощь (хелп, help, помощь) - список команд", "info (инфо) - информация о боте", True)]
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
-            embed.set_author(name="TurboTemuch Bot", icon_url=self.guild.icon_url)
         await channel.send(embed=embed)
 
     @command(name="info", aliases=["инфо"])
@@ -39,12 +38,9 @@ class Fun(Cog):
         fields = [("Бот TurboTemuch успешно запущен!", "Bot created and coded by TurboTemuch#7375", True)]
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=inline)
-            embed.set_author(name="TurboTemuch1 Bot", icon_url=self.guild.icon_url)
+            embed.set_author(name="Bot TurboTemuch1")
             embed.set_footer(text="Запущен в работу")
-            embed.set_thumbnail(url=self.guild.icon_url)
         await channel.send(embed=embed)
-            
-        await channel.send(file=File("./data/images/APNG-cube.png"))
 
     @Cog.listener()
     async def on_ready(self):
