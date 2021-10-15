dddee0669b6728491c731d54b6b28ef91a9f8331
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2021-10-15 11:31:32 +0300

    added help and info

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -1,7 +1,9 @@
 from random import choice
 from typing import Optional
 
+from discord import Intents
 from discord import Member
+from discord import Embed, File
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
@@ -17,6 +19,30 @@ class Fun(Cog):
     @command(name="slap", aliases=["hit","ударить"])
     async def slap_member(self, ctx, member:Member, *, reason: Optional[str] = "no reason"):
         await ctx.send(f"{ctx.author.name} ударил {member.mention} по причине: {reason}!")
+      
+    @command(name="help", aliases=["помощь","Помощь","хелп"])
+    async def help(self, channel):
+        embed = Embed(title="Команды:", colour=0x00FF00,
+                          timestamp=datetime.utcnow())
+        fields = [("Префикс: `>`","Модуль fun:", "Связь (Бот, связь, бот) - проверка связи с ботом", "slap (hit, ударить) `>slap @участник причина`", "Помощь (хелп, help, помощь) - список команд", "info (инфо) - информация о боте", True)]
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="TurboTemuch Bot", icon_url=self.guild.icon_url)
+        await channel.send(embed=embed)
+
+    @command(name="info", aliases=["инфо"])
+    async def info(self, channel):
+        embed = Embed(title="[В СЕТИ]", colour=0x00FF00,
+                          timestamp=datetime.utcnow())
+        fields = [("Бот TurboTemuch успешно запущен!", "Bot created and coded by TurboTemuch#7375", True)]
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="TurboTemuch1 Bot", icon_url=self.guild.icon_url)
+            embed.set_footer(text="Запущен в работу")
+            embed.set_thumbnail(url=self.guild.icon_url)
+        await channel.send(embed=embed)
+            
+        await channel.send(file=File("./data/images/APNG-cube.png"))
 
     @Cog.listener()
     async def on_ready(self):
