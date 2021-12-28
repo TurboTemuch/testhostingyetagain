b1224e170ad0d3cb3ec6d25b1009af44d8250b7a
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2021-12-28 15:57:30 +0300

    Update __init__.py

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -95,7 +95,7 @@ class Bot(BotBase):
         if isinstance(exc, BadArgument):
             await ctx.send(':x: Введён некорректный аргумент.')
             
-         if isinstance(exc, CommandOnCooldown):
+        if isinstance(exc, CommandOnCooldown):
             await ctx.send(f":clock1: Вы достигли кулдауна команды! Вы сможете использовать её вновь через {exc.retry_after:,.2f} секунд.")
             
         if isinstance(exc, ExtensionFailed):
