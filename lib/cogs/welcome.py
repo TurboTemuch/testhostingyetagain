9ed4a7de12af04d1342a3c8a697cfff151422251
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2021-12-29 17:58:56 +0300

    Add files via upload

---
+++ lib/cogs/welcome.py
@@ -0,0 +1,28 @@
+from discord.errors import Forbidden
+from discord.ext.commands import Cog, command
+
+class Welcome(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("welcome")
+    
+    @Cog.listener()
+    async def on_member_join(self, member):
+        db.execute("INSERT INTO exp (UserID) VALUES (?)", member.id)
+        await self.bot.get_channel(821776825616695316).send(f"Добро пожаловать на сервер **{member.guild.name}** {member.mention}! Не забудьте прочитать правила в канале <#742651335262076938>!")
+        try:
+            await member.send(f"Добро пожаловать на сервер **{member.guild.name}**! Чувствуйте себя, как дома!")
+        except Forbidden:
+            pass
+
+    @Cog.listener()
+    async def on_member_leave(self, member):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(Welcome(bot))
