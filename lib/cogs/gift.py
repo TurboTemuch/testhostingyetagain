38b9f2a6dfb108e39bbcdc67cd4cee6d3a9549d3
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2022-01-07 21:37:15 +0300

    Update gift.py

--- lib/cogs/gift.py
+++ lib/cogs/gift.py
@@ -35,217 +35,217 @@ class Gift(Cog):
     @commands.has_role("Главный Санта")
     @has_permissions(kick_members=True, administrator=True)
     @cooldown(1, 5, BucketType.user)
-    async def gift(self, ctx, *, time: Optional[str] = "30s" ):
+    async def gift(self, ctx) #, *, time: Optional[str] = "30s" ):
       """Команда для сброса подарка."""
+        await ctx.send(":no-entry: Эта команда была отключена.")
+#       def check(m):
+#         return m.author == ctx.author and m.channel == ctx.channel
+
+#       timeout = convert(time)
+#       if timeout == -1:
+#         await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
+#         return
+#       elif timeout == -2:
+#         await ctx.send(f"Некорректно введено время. Введите целое число.")
+#         return
   
-      def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
+#       prize = ":gift:"
 
-      timeout = convert(time)
-      if timeout == -1:
-        await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
-        return
-      elif timeout == -2:
-        await ctx.send(f"Некорректно введено время. Введите целое число.")
-        return
-  
-      prize = ":gift:"
+#     #   await ctx.send(f"Розыгрыш будет проведён в {channel.mention} и итоги будут опубликованы через {answers[1]} секунд!")
 
-    #   await ctx.send(f"Розыгрыш будет проведён в {channel.mention} и итоги будут опубликованы через {answers[1]} секунд!")
+#       embed = discord.Embed(title = "Появился подарок!", description = f"{prize}", color = 0xFFFFFF)
 
-      embed = discord.Embed(title = "Появился подарок!", description = f"{prize}", color = 0xFFFFFF)
+#       embed.set_footer(text = f"Открывается через {time}!")
 
-      embed.set_footer(text = f"Открывается через {time}!")
+#       my_msg = await ctx.send(embed = embed)
 
-      my_msg = await ctx.send(embed = embed)
+#       await my_msg.add_reaction("🎁")
 
-      await my_msg.add_reaction("🎁")
-
-      await ctx.message.delete()
+#       await ctx.message.delete()
       
-      await asyncio.sleep(timeout)
+#       await asyncio.sleep(timeout)
 
-      cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
-      print(cache_msg.reactions)
+#       cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
+#       print(cache_msg.reactions)
       
-      users = await cache_msg.reactions[0].users().flatten()
-      users.pop(users.index(self.bot.user))
+#       users = await cache_msg.reactions[0].users().flatten()
+#       users.pop(users.index(self.bot.user))
 
-      winner = random.choice(users)
+#       winner = random.choice(users)
 
-      await ctx.send(f"Подарок забрал {winner.mention}! <@&880169755397464064> скоро выдаст приз.")
+#       await ctx.send(f"Подарок забрал {winner.mention}! <@&880169755397464064> скоро выдаст приз.")
 
     @commands.command(name="эпикподарок", aliases=["epicdrop","epicgift"], description="Команда для сброса эпического подарка.")
     @commands.has_role("Главный Санта")
     @has_permissions(kick_members=True, administrator=True)
     @cooldown(1, 5, BucketType.user)
-    async def epicgift(self, ctx, *, time: Optional[str] = "45s" ):
+    async def epicgift(self, ctx) #, *, time: Optional[str] = "45s" ):
       """Команда для сброса эпического подарка."""
-        
-      def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
+        await ctx.send(":no-entry: Эта команда была отключена.")    
+#       def check(m):
+#         return m.author == ctx.author and m.channel == ctx.channel
 
-      timeout = convert(time)
-      if timeout == -1:
-        await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
-        return
-      elif timeout == -2:
-        await ctx.send(f"Некорректно введено время. Введите целое число.")
-        return
+#       timeout = convert(time)
+#       if timeout == -1:
+#         await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
+#         return
+#       elif timeout == -2:
+#         await ctx.send(f"Некорректно введено время. Введите целое число.")
+#         return
   
-      prize = ":gift:"
+#       prize = ":gift:"
 
-    #   await ctx.send(f"Розыгрыш будет проведён в {channel.mention} и итоги будут опубликованы через {answers[1]} секунд!")
+#     #   await ctx.send(f"Розыгрыш будет проведён в {channel.mention} и итоги будут опубликованы через {answers[1]} секунд!")
 
-      embed = discord.Embed(title = "Появился *эпический* подарок!", description = f"{prize}", color = 0x7E13AE)
+#       embed = discord.Embed(title = "Появился *эпический* подарок!", description = f"{prize}", color = 0x7E13AE)
 
-      embed.set_footer(text = f"Открывается через {time}!")
+#       embed.set_footer(text = f"Открывается через {time}!")
 
-      my_msg = await ctx.send(embed = embed)
+#       my_msg = await ctx.send(embed = embed)
 
-      await my_msg.add_reaction("🎁")
+#       await my_msg.add_reaction("🎁")
 
-      await ctx.message.delete()
+#       await ctx.message.delete()
       
-      await asyncio.sleep(timeout)
+#       await asyncio.sleep(timeout)
 
-      cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
-      print(cache_msg.reactions)
+#       cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
+#       print(cache_msg.reactions)
       
-      users = await cache_msg.reactions[0].users().flatten()
-      users.pop(users.index(self.bot.user))
+#       users = await cache_msg.reactions[0].users().flatten()
+#       users.pop(users.index(self.bot.user))
 
-      winner = random.choice(users)
+#       winner = random.choice(users)
 
-      await ctx.send(f"{winner.mention} забрал *эпический* подарок! <@&880169755397464064> скоро выдаст приз.")
+#       await ctx.send(f"{winner.mention} забрал *эпический* подарок! <@&880169755397464064> скоро выдаст приз.")
 
     @commands.command(name="мифподарок", aliases=["mythdrop","mythgift"], description="Команда для сброса мифического подарка.")
     @commands.has_role("Главный Санта")
     @has_permissions(kick_members=True, administrator=True)
     @cooldown(1, 5, BucketType.user)
-    async def mythgift(self, ctx, *, time: Optional[str] = "60s" ):
+    async def mythgift(self, ctx) #, *, time: Optional[str] = "60s" ):
       """Команда для сброса мифического подарка."""
+        await ctx.send(":no-entry: Эта команда была отключена.")
+#       def check(m):
+#         return m.author == ctx.author and m.channel == ctx.channel
 
-      def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
-
-      timeout = convert(time)
-      if timeout == -1:
-        await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
-        return
-      elif timeout == -2:
-        await ctx.send(f"Некорректно введено время. Введите целое число.")
-        return
+#       timeout = convert(time)
+#       if timeout == -1:
+#         await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
+#         return
+#       elif timeout == -2:
+#         await ctx.send(f"Некорректно введено время. Введите целое число.")
+#         return
   
-      prize = ":gift:"
+#       prize = ":gift:"
 
-      embed = discord.Embed(title = "Появился **МИФИЧЕСКИЙ** подарок!", description = f"{prize}", color = 0xFF0000)
+#       embed = discord.Embed(title = "Появился **МИФИЧЕСКИЙ** подарок!", description = f"{prize}", color = 0xFF0000)
 
-      embed.set_footer(text = f"Открывается через {time}!")
+#       embed.set_footer(text = f"Открывается через {time}!")
 
-      my_msg = await ctx.send(embed = embed)
+#       my_msg = await ctx.send(embed = embed)
 
-      await my_msg.add_reaction("🎁")
+#       await my_msg.add_reaction("🎁")
 
-      await ctx.message.delete()
+#       await ctx.message.delete()
       
-      await asyncio.sleep(timeout)
+#       await asyncio.sleep(timeout)
 
-      cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
-      print(cache_msg.reactions)
+#       cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
+#       print(cache_msg.reactions)
       
-      users = await cache_msg.reactions[0].users().flatten()
-      users.pop(users.index(self.bot.user))
+#       users = await cache_msg.reactions[0].users().flatten()
+#       users.pop(users.index(self.bot.user))
 
-      winner = random.choice(users)
+#       winner = random.choice(users)
 
-      await ctx.send(f"{winner.mention} забрал **МИФИЧЕСКИЙ** подарок! <@&880169755397464064> скоро выдаст приз.")
+#       await ctx.send(f"{winner.mention} забрал **МИФИЧЕСКИЙ** подарок! <@&880169755397464064> скоро выдаст приз.")
 
     @commands.command(name="легаподарок", aliases=["legenddrop","legendgift","legendarygift","legendarydrop"], description="Команда для сброса легендарного подарка.")
     @commands.has_role("Главный Санта")
     @has_permissions(kick_members=True, administrator=True)
     @cooldown(1, 5, BucketType.user)
-    async def legendarygift(self, ctx, *, time: Optional[str] = "90s" ):
+    async def legendarygift(self, ctx) #, *, time: Optional[str] = "90s" ):
       """Команда для сброса легендарного подарка."""
-        
-      def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
+        await ctx.send(":no-entry: Эта команда была отключена.")
+#       def check(m):
+#         return m.author == ctx.author and m.channel == ctx.channel
 
-      timeout = convert(time)
-      if timeout == -1:
-        await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
-        return
-      elif timeout == -2:
-        await ctx.send(f"Некорректно введено время. Введите целое число.")
-        return
+#       timeout = convert(time)
+#       if timeout == -1:
+#         await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
+#         return
+#       elif timeout == -2:
+#         await ctx.send(f"Некорректно введено время. Введите целое число.")
+#         return
   
-      prize = ":gift:"
+#       prize = ":gift:"
 
-    #   await ctx.send(f"Розыгрыш будет проведён в {channel.mention} и итоги будут опубликованы через {answers[1]} секунд!")
+#     #   await ctx.send(f"Розыгрыш будет проведён в {channel.mention} и итоги будут опубликованы через {answers[1]} секунд!")
 
-      embed = discord.Embed(title = ":crown:Появился **ЛЕГЕНДАРНЫЙ** подарок!:crown: ", description = f"{prize}", color = 0xFFC600)
+#       embed = discord.Embed(title = ":crown:Появился **ЛЕГЕНДАРНЫЙ** подарок!:crown: ", description = f"{prize}", color = 0xFFC600)
 
-      embed.set_footer(text = f"Открывается через {time}!")
+#       embed.set_footer(text = f"Открывается через {time}!")
 
-      my_msg = await ctx.send(embed = embed)
+#       my_msg = await ctx.send(embed = embed)
 
-      await my_msg.add_reaction("🎁")
+#       await my_msg.add_reaction("🎁")
 
-      await ctx.message.delete()
+#       await ctx.message.delete()
       
-      await asyncio.sleep(timeout)
+#       await asyncio.sleep(timeout)
 
-      cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
-      print(cache_msg.reactions)
+#       cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
+#       print(cache_msg.reactions)
       
-      users = await cache_msg.reactions[0].users().flatten()
-      users.pop(users.index(self.bot.user))
+#       users = await cache_msg.reactions[0].users().flatten()
+#       users.pop(users.index(self.bot.user))
 
-      winner = random.choice(users)
+#       winner = random.choice(users)
 
-      await ctx.send(f"{winner.mention} забрал :crown: **ЛЕГЕНДАРНЫЙ**:crown:  подарок! <@&880169755397464064> скоро выдаст приз.")
+#       await ctx.send(f"{winner.mention} забрал :crown: **ЛЕГЕНДАРНЫЙ**:crown:  подарок! <@&880169755397464064> скоро выдаст приз.")
       
     @commands.command(name="экзаподарок", aliases=["exoticdrop","exoticgift"], description="Команда для сброса экзотического подарка.")
     @commands.has_role("Главный Санта")
     @has_permissions(kick_members=True, administrator=True)
     @cooldown(1, 5, BucketType.user)
-    async def mythgift(self, ctx, *, time: Optional[str] = "60s" ):
+    async def mythgift(self, ctx) #, *, time: Optional[str] = "60s" ):
       """Команда для сброса экзотического подарка."""
+        await ctx.send(":no-entry: Эта команда была отключена.")
+#       def check(m):
+#         return m.author == ctx.author and m.channel == ctx.channel
 
-      def check(m):
-        return m.author == ctx.author and m.channel == ctx.channel
-
-      timeout = convert(time)
-      if timeout == -1:
-        await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
-        return
-      elif timeout == -2:
-        await ctx.send(f"Некорректно введено время. Введите целое число.")
-        return
+#       timeout = convert(time)
+#       if timeout == -1:
+#         await ctx.send(f"Некорректно введено обозначение. Используйте (s|m|h|d).")
+#         return
+#       elif timeout == -2:
+#         await ctx.send(f"Некорректно введено время. Введите целое число.")
+#         return
   
-      prize = ":gift:"
+#       prize = ":gift:"
 
-      embed = discord.Embed(title = "Появился **ЭКЗОТИЧЕСКИЙ** подарок!", description = f"{prize}", color = 0x24FF00)
+#       embed = discord.Embed(title = "Появился **ЭКЗОТИЧЕСКИЙ** подарок!", description = f"{prize}", color = 0x24FF00)
 
-      embed.set_footer(text = f"Открывается через {time}!")
+#       embed.set_footer(text = f"Открывается через {time}!")
 
-      my_msg = await ctx.send(embed = embed)
+#       my_msg = await ctx.send(embed = embed)
 
-      await my_msg.add_reaction("🎁")
+#       await my_msg.add_reaction("🎁")
 
-      await ctx.message.delete()
+#       await ctx.message.delete()
       
-      await asyncio.sleep(timeout)
+#       await asyncio.sleep(timeout)
 
-      cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
-      print(cache_msg.reactions)
+#       cache_msg = discord.utils.get(self.bot.cached_messages, id=my_msg.id) #or client.messages depending on your variable
+#       print(cache_msg.reactions)
       
-      users = await cache_msg.reactions[0].users().flatten()
-      users.pop(users.index(self.bot.user))
+#       users = await cache_msg.reactions[0].users().flatten()
+#       users.pop(users.index(self.bot.user))
 
-      winner = random.choice(users)
+#       winner = random.choice(users)
 
-      await ctx.send(f"{winner.mention} забрал **ЭКЗОТИЧЕСКИЙ** подарок! <@&880169755397464064> скоро выдаст приз.")
+#       await ctx.send(f"{winner.mention} забрал **ЭКЗОТИЧЕСКИЙ** подарок! <@&880169755397464064> скоро выдаст приз.")
       
     @commands.command(name="incoming", aliases=["dropsoon"], description="Объявление о скором появлении подарков.")
     @commands.has_role("Главный Санта")
@@ -253,9 +253,10 @@ class Gift(Cog):
     @cooldown(1, 30, BucketType.user)
     async def incoming(self, ctx):
       """Объявление о скором появлении подарков."""
-      await ctx.message.delete()
-      embed = discord.Embed(title = "Cкоро будет сброшен подарок!", color = 0xFFC600)
-      await ctx.send(embed = embed)
+      await ctx.send(":no-entry: Эта команда была отключена.")
+#       await ctx.message.delete()
+#       embed = discord.Embed(title = "Cкоро будет сброшен подарок!", color = 0xFFC600)
+#       await ctx.send(embed = embed)
       
     @commands.command(name="exoticalincoming", aliases=["exoticaldropsoon"], description="Объявление о скором появлении экзотического подарка.")
     @commands.has_role("Главный Санта")
@@ -263,9 +264,10 @@ class Gift(Cog):
     @cooldown(1, 30, BucketType.user)
     async def incoming(self, ctx):
       """Объявление о скором появлении подарков."""
-      await ctx.message.delete()
-      embed = discord.Embed(title = "Cкоро будет сброшен **ЭКЗОТИЧЕСКИЙ** подарок!", color = 0x24FF00)
-      await ctx.send(embed = embed)
+        await ctx.send(":no-entry: Эта команда была отключена.")
+#       await ctx.message.delete()
+#       embed = discord.Embed(title = "Cкоро будет сброшен **ЭКЗОТИЧЕСКИЙ** подарок!", color = 0x24FF00)
+#       await ctx.send(embed = embed)
       
     @Cog.listener()
     async def on_ready(self):
