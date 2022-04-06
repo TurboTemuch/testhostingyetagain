be9702d9f141b76344f72a7a0d9a86498178fbdd
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2022-04-06 12:31:44 +0300

    Update main.py

--- lib/cogs/main.py
+++ lib/cogs/main.py
@@ -30,8 +30,8 @@ class Main(Cog):
     @cooldown(1, 20, BucketType.user)
     async def tts(self, ctx, *, text:str):
         """Скажите что-нибудь от лица бота. (🔒Необходима роль TurboBot1-Access)"""
-        member = ctx.author.nickname
-        await ctx.send(f"{text} \n  *Автор: {member}*")
+        memb = ctx.author.nick
+        await ctx.send(f"{text} \n  *Автор: {memb}*")
         await ctx.message.delete()
 
     @command(name="t", aliases=["tell", "текст", "с"], description="Скажите что-нибудь от лица бота. (🔒Доступна только владельцу, без кулдауна)")
