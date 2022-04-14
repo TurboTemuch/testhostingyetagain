b3ad2ca7e8d22088d1faaf37346942b97c06a903
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2022-04-14 11:07:26 +0300

    Create exp.py

---
+++ lib/cogs/exp.py
@@ -0,0 +1,32 @@
+from discord.ext.commands import Cog
+from discord.ext.commands import CheckFailure
+from discord.ext.commands import command, has_permissions
+
+from ..db import db
+
+class Cmds(Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @command(name="префикс")
+  @has_permissions(manage_guild=True)
+  async def changeprefix(self, ctx, newprefix: str):
+    if len(newprefix)>2:
+      await ctx.send("Префикс не может быть длиннее 2-х символов!")
+    
+    else:
+      db.execute("UPDATE guilds SET Prefix = ? WHERE GuildID = ?", newprefix, ctx.guild.id)
+      await ctx.send(f"Префикс изменён на `{newprefix}`.")
+  
+  @changeprefix.error
+  async def changeprefix_error(self, ctx, exc):
+    if isinstance(exc, CheckFailure):
+      await ctx.send(":lock: Необходимо право управления сервером.")
+
+  @Cog.listener()
+  async def on_ready(self):
+    if not self.bot.ready:
+      self.bot.cogs_ready.ready_up("exp")
+
+def setup(bot):
+  bot.add_cog(Exp(bot))
