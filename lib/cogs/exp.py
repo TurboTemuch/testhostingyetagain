5d691695216f86f7ee667fa7eb0f24f03eb92e10
TurboTemuch <77197879+TurboTemuch@users.noreply.github.com>
2022-04-14 11:28:15 +0300

    Update exp.py

--- lib/cogs/exp.py
+++ lib/cogs/exp.py
@@ -1,6 +1,7 @@
 from discord.ext.commands import Cog
 from discord.ext.commands import CheckFailure
 from discord.ext.commands import command, has_permissions
+from random import randint
 
 from ..db import db
 
@@ -23,9 +24,32 @@ class Exp(Cog):
     if isinstance(exc, CheckFailure):
       await ctx.send(":lock: Необходимо право управления сервером.")
 
+  async def process_xp(self, message):
+    xp, lcl, xplock = db.record("SELECT XP, Level, XPLock FROM exp WHERE UserID = ?", message.author.id)
+
+    if datetime.fromisoformat(xplock) > datetime.utcnow():
+      await self.add_xp(message, xp, lvl)
+   
+  async def add_xp(self, message, xp, lvl):
+    xp_to_add = randint(5, 10)
+    
+    if (new_lvl := int(((xp+xp_add)//42) ** 0.55)) > lvl:
+      db.execute("UPDATE exp SET XP = XP + ?, Level = ? WHERE UserID = ?", message.author.id)
+    
+    db.execute("UPDATE exp SET XP = XP + ? WHERE UserID = ?", xp_add, new_lvl, message.author.id)
+    
+    if new_lvl > lvl
+      await self.channellvlup.send(f"Поздравляю {message.author.mention} с получением уровня {new_lvl:,}!")
+    
+  @Cog.listener()
+  async def on_message(self, message):
+    if not message.author.bot:
+      await self.process_xp(message)
+  
   @Cog.listener()
   async def on_ready(self):
     if not self.bot.ready:
+      self.channellvlup = self.bot.get_channel(964079078665756712)
       self.bot.cogs_ready.ready_up("exp")
 
 def setup(bot):
